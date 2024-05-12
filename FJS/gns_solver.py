ff220d119687907b746cd88c837021a0267ecb9e
Anas Neumann <anas.neumann.isamm@gmail.com>
2024-05-12 08:11:42 -0400

    New function for past utilization of resource and remaining load

--- FJS/gns_solver.py
+++ FJS/gns_solver.py
@@ -8,16 +8,36 @@ TEST_INSTANCES_PATH = './FJS/instances/test/'
 NOT_SCHEDULED = 0
 SCHEDULED = 1
 INITIAL_MAKESPAN = 0
-OPERATION_FEATURES = {"status": 0, "num_resources": 1, "duration": 2, "start": 3, "num_ops": 4, "job_completion": 5} # TODO 1. NEED TO DO THE POSITION AS A PERCENTAGE
-RESOURCE_FEATURES = {"available": 0, "num_ops": 1, "utilization": 2}
+OPERATION_FEATURES = {"status": 0, "remaining_neighboring_resources": 1, "duration": 2, "start": 3, "remaining_neighboring_ops": 4, "current_job_completion": 5, "position": 6}
+RESOURCE_FEATURES = {"available_time": 0, "remaining_neighboring_ops": 1, "past_utilization_rate": 2, "remaining_load_percentage": 3}
 
-# TODO 2. MAYBE WE WILL HAVE TO CHANGE NEIGHBOORING OPERATIONS AND RESSOURCES COMPUTATION/METHOD WHEN SCHEDULED!!
-# TODO 3. NEED TO DO THE RESOURCE UTILIZATION METHOD
-# TODO 4?. NEED FOR GLOBAL INFORMATION: NBR RESOURCES, NBR JOBS, NBR OPERATIONS, etc.?
+# TODO WE WILL HAVE TO CHANGE NEIGHBOORING OPERATIONS AND RESSOURCES COMPUTATION/METHOD WHEN SCHEDULED!!
+# TODO FUNCTION TO BUILD THE SOLUTION
+# TODO FINISH THE compute_remaining_load function
 
 #====================================================================================================================
 # =*= FONCTIONS TO BUILD AND UPDATE THE RAW GRAPH =*=
 #====================================================================================================================
+
+# Compute the global load before having the graph
+def compute_global_load(id, jobs):
+    total_load = 0
+    resource_load = 0
+    for job in jobs:
+        for operation in job:
+            total_load += operation[OP_STRUCT["duration"]]
+            if operation[OP_STRUCT["resource_type"]] == id:
+                resource_load += operation[OP_STRUCT["duration"]]
+    return 0 if total_load<=0 else (resource_load*1.0)/total_load
+
+# TODO 
+def compute_remaining_load(graph, jobs, operations2graph):
+    total_remaining_load = 0
+    resource_remaining_load = 0
+    for job_id, job in enumerate(jobs):
+        for op_id, operation in enumerate(job):
+            print("hello")
+    return 0 
 
 # Get the job completion time in the current schedule
 def job_current_completion(graph, operations_idx):
@@ -29,8 +49,15 @@ def job_current_completion(graph, operations_idx):
     return completion
 
 # Get the utilization rate of a specific resource
-def res_utilization(resource, current_time):
-    return 0
+def res_utilization(graph, operations_idx):
+    completion = 0
+    utilized_time = 0
+    for id in operations_idx:
+        if is_scheduled(graph, id):
+            _,duration,end_time = get_operation_dates(graph, id)
+            completion = max(completion, end_time)
+            utilized_time += duration
+    return 0 if completion<=0 else (utilized_time*1.0)/completion
 
 # Get the number of operations that need a type of resources
 def ops_by_resource(type, jobs):
@@ -81,17 +108,21 @@ def instance_to_graph(instance):
     graph = HeteroData()
     resources = instance['resources']
     jobs = instance['jobs']
-    zero_features = torch.zeros((1, 6), dtype=torch.int64)
+    size = instance['size']
+    total_resources = instance['nb_res']
+    zero_features = torch.zeros((1, 7), dtype=torch.int64)
     graph = add_node(graph, 'operation', zero_features) # start node
     operations2graph = [] 
     resources_by_type = []
+    operations_of_resources = [[] for _ in range(total_resources)]
 
     # I. Resource nodes
     resource_id = 0
     for type, quantity in enumerate(resources):
         res_of_type = []
-        for resource in range(quantity):
-            graph = add_node(graph, 'resource', torch.tensor([[INITIAL_MAKESPAN, ops_by_resource(type, jobs), res_utilization(resource, INITIAL_MAKESPAN)]]))
+        load = compute_global_load(type, jobs)
+        for _ in range(quantity):
+            graph = add_node(graph, 'resource', torch.tensor([[INITIAL_MAKESPAN, ops_by_resource(type, jobs), INITIAL_MAKESPAN, load]]))
             res_of_type.append(resource_id)
             resource_id += 1
         resources_by_type.append(res_of_type)
@@ -102,8 +133,8 @@ def instance_to_graph(instance):
         first_op_id = op_id
         prec_operations_idx = []
         ops2graph = []
-        for operation in job:
-            graph = add_node(graph, 'operation', torch.tensor([[NOT_SCHEDULED, resources[operation[OP_STRUCT["resource_type"]]], operation[OP_STRUCT["duration"]], estimate_start_time(graph, prec_operations_idx), len(job), INITIAL_MAKESPAN]]))
+        for position, operation in enumerate(job):
+            graph = add_node(graph, 'operation', torch.tensor([[NOT_SCHEDULED, resources[operation[OP_STRUCT["resource_type"]]], operation[OP_STRUCT["duration"]], estimate_start_time(graph, prec_operations_idx), len(job), INITIAL_MAKESPAN, position]]))
             prec_operations_idx.append(op_id)
             ops2graph.append(op_id)
             if op_id > first_op_id: # III. Precedence edges (1st relation)
@@ -117,24 +148,25 @@ def instance_to_graph(instance):
     for job in jobs:
         graph = add_edge(graph, 'start', 'to', 'operation', torch.tensor([[0], [op_id]], dtype=torch.long))
         op_id += len(job)
-        graph = add_edge(graph, 'operation', 'to', 'end', torch.tensor([[op_id-1], [instance['size']+1]], dtype=torch.long))
+        graph = add_edge(graph, 'operation', 'to', 'end', torch.tensor([[op_id-1], [size+1]], dtype=torch.long))
 
     # V. Resource compatibility and use edges (2nd relation)
     for job_id, job in enumerate(jobs):
         for op_id, operation in enumerate(job):
             op2graph_id = operations2graph[job_id][op_id]
             for res2graph_id in resources_by_type[operation[OP_STRUCT["resource_type"]]]:
+                operations_of_resources[res2graph_id].append(op2graph_id)
                 graph = add_edge(graph, 'operation', 'uses', 'resource', torch.tensor([[op2graph_id], [res2graph_id]], dtype=torch.long))            
-    return graph, operations2graph, resources_by_type
+    return graph, operations2graph, resources_by_type, operations_of_resources
 
 # Display the details of the current graph
 def display_graph(graph):
-    print("Graph overview: " + str(graph))
+    print("Graph_overview: " + str(graph))
     print("Resources: " + str(graph['resource']))
     print("Operations: " + str(graph['operation']))
-    print("Precedence relations: " + str(graph['operation', 'precedence', 'operation']))
-    print("Link with dummy start node: " + str(graph['start', 'to', 'operation']))
-    print("Link with dummy end node: " + str(graph['operation', 'to', 'end']))
+    print("Precedence_relations: " + str(graph['operation', 'precedence', 'operation']))
+    print("Link_with_dummy_start_node: " + str(graph['start', 'to', 'operation']))
+    print("Link_with_dummy_end_node: " + str(graph['operation', 'to', 'end']))
     print("Requirements: " + str(graph['operation', 'uses', 'resource']))
 
 #====================================================================================================================
@@ -152,5 +184,5 @@ def display_graph(graph):
 train_instances = load_instances(TRAIN_INSTANCES_PATH)
 test_instances = load_instances(TEST_INSTANCES_PATH)
 print(train_instances[0])
-graph,_,_ = instance_to_graph(train_instances[0])
+graph,_,_,_ = instance_to_graph(train_instances[0])
 display_graph(graph)
--- FJS/instance_generator.py
+++ FJS/instance_generator.py
@@ -20,8 +20,11 @@ for i in range(NB_TRAIN_INSTANCES + NB_TEST_INSTANCES):
 
     # RESOURCES GENERATION
     resources = []
+    total_res = 0
     for _ in range(random.randint(MIN_TYES_OF_RESOURCES, MAX_TYES_OF_RESOURCES)):
-        resources.append(random.randint(1, MAX_RESOURCES_BY_TYPE))
+        nb_res_by_type = random.randint(1, MAX_RESOURCES_BY_TYPE)
+        resources.append(nb_res_by_type)
+        total_res += nb_res_by_type
 
     # JOB GENERATION
     jobs = []
@@ -36,7 +39,7 @@ for i in range(NB_TRAIN_INSTANCES + NB_TEST_INSTANCES):
         jobs.append(j)
 
     # SAVE THE INSTANCE
-    instance = {"resources": resources, "jobs": jobs, "size": total_ops}
+    instance = {"resources": resources, "jobs": jobs, "size": total_ops, "nb_res": total_res}
     folder = "train" if i < NB_TRAIN_INSTANCES else "test"
     with open('./FJS/instances/'+folder+'/instance_'+str(i)+'.pkl', 'wb') as f:
         pickle.dump(instance, f)
