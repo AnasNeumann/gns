f41d2bd0db5ccbaf89c968dc189878ea00329600
Anas Neumann <anas.neumann.isamm@gmail.com>
2024-10-13 10:42:30 -0400

    reconstruct log.sh during the purge stage

--- jobs/scripts/0_run_purge.sh
+++ jobs/scripts/0_run_purge.sh
@@ -2,4 +2,6 @@
 echo "Removing all results and recreating folders..."
 rm -rf ../../data/out/
 rm -rf ../../data/models/
+rm -f ../../log.sh
 mkdir ../../data/out/ ../../data/models/
+touch ../../log.sh
