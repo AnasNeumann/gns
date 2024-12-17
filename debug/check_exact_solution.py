2babd68425b73d50bd258f9e61ef73853639d056
Anas Neumann <anas.neumann.isamm@gmail.com>
2024-12-17 11:35:32 +0100

    Create check_exact_solution.py

---
+++ debug/check_exact_solution.py
@@ -0,0 +1,6 @@
+# =====================================================================
+# =*= Check if a solution is feasible according to the exact solver =*=
+# =====================================================================
+__author__ = "Anas Neumann - anas.neumann@polymtl.ca"
+__version__ = "1.0.0"
+__license__ = "Apache 2.0 License"
